f25bf4b67407078f91f613d2ee2878a20f216036
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2025-10-26 13:36:15 +0000

    Update

--- scripts/full-backup.sh
+++ scripts/full-backup.sh
@@ -1,9 +1,8 @@
 duplicity backup --no-encryption \
   --include /home/archboom/Documents \
   --include /home/archboom/Downloads \
-  --include /home/archboom/Pictures \
+  --include /home/archboom/Pictures/Screenshots \
   --include /home/archboom/dotfiles \
   --include /home/archboom/.config \
   --exclude '**' \
   / "file:///mnt/Backups/Linux backups/"
-
--- scripts/half-backup.sh
+++ scripts/half-backup.sh
@@ -1,8 +1,7 @@
 duplicity incremental --no-encryption \
   --include /home/archboom/Documents \
   --include /home/archboom/Downloads \
-  --include /home/archboom/Pictures \
-  --include /home/archboom/dotfiles \
+  --include /home/archboom/Pictures/Screenshots \
   --include /home/archboom/dotfiles \
   --exclude '**' \
   / "file:///mnt/Backups/Linux backups/"
