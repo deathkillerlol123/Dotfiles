8591ffe6123bd0a9f69d6e149fd95e1a4607110d
deathkillerlol123 <81639935+deathkillerlol123@users.noreply.github.com>
2025-10-30 17:07:17 +0000

    Update

--- scripts/waybar.sh
+++ scripts/waybar.sh
@@ -1,2 +1,1 @@
-waybar -c ~/dotfiles/.config/waybar/config-right -s ~/dotfiles/.config/waybar/style-right.css &
-waybar -c ~/dotfiles/.config/waybar/config-left -s ~/dotfiles/.config/waybar/style-left.css &
+waybar -c ~/dotfiles/.config/waybar/config -s ~/dotfiles/.config/waybar/style.css &
